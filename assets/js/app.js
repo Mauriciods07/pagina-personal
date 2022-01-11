f2ffb41dbb181ed388fd16d6b689612aa47a269d
Mauricio Díaz <mauricio.idiazs@gmail.com>
2022-01-10 19:47:29 -0600

    Complementar sobre mi

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,7 +46,7 @@ let duration = 3000;
     })
 
     sr.reveal('.projects-section', {
-        duration: duration,
+        duration: 2000,
         origin: 'right',
         distance: '500px'
     })
@@ -56,3 +56,46 @@ let duration = 3000;
         origin: 'bottom',
         distance: '100px'
     })
+
+/* Lightbox - detener video al cerrarlo*/
+/*  var stopVideo = () => {
+    var iframes = document.querySelectorAll('iframe');
+    Array.prototype.forEach.call(iframes, iframe => {
+        iframe.contentWindow.postMessage(JSON.stringify({ event: 'command',
+    func: 'stopVideo' }), '*');
+    });
+} 
+
+function pauseVideos() 
+    { 
+        $('iframe').contents().find('video').each(function () 
+        {
+            this.currentTime = 0;
+            this.pause();
+        });
+    }
+
+    let videoStopper = function(id) {
+        let containerElement = document.getElementById(id);
+        let iframe_tag = containerElement.querySelector( 'iframe');
+        let video_tag = containerElement.querySelector( 'video' );
+        if ( iframe_tag) {
+            let iframeSrc = iframe_tag.src;
+            iframe_tag.src = iframeSrc; 
+        }
+        if ( video_tag) {
+            video_tag.pause();
+        }} */
+
+        /* const obtenerURL = () =>{
+            const pagina = document.getElementById('iframe-Safari')
+            let direccionWeb = document.getElementById('urlBuscar').value;
+            console.log(direccionWeb)
+            pagina.src = direccionWeb;
+        } */
+
+        function obtenerURL(id) {
+            const pagina = document.getElementById(id);
+            pagina.src = pagina.src;
+        }
+        
